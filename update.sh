9c5bd0eb3f15bd4d66e1f82c315dbe2cca635ced
Theo Brigitte <theo.brigitte@gmail.com>
2019-03-05 12:54:07 +0100

    update.sh: comments

--- update.sh
+++ update.sh
@@ -9,7 +9,11 @@ SCRIPT_DIR="$( cd "$( dirname "$0" )" && pwd )"
 # vim commands like :DoSometing can be written as vim +DoSomething +qall
 
 set -x
+
+# update vim-go required binaries
 vim +GoUpdateBinaries +qall
+
+# update youcompletme dependencies
 python3 bundle/youcompleteme/install.py \
 	--go-completer \
 	--ts-completer
